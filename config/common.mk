tavukkdoner/vendor_evolution

config: Add debug persist prop

config/common.mk
@@ -54,10 +54,12 @@ endif
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=0
+PRODUCT_PRODUCT_PROPERTIES += persist.sys.evox_debug_enabled=1
 else
 ifdef WITH_ADB_INSECURE
 # Forcebly disable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=0
+PRODUCT_PRODUCT_PROPERTIES += persist.sys.evox_debug_enabled=1
 else
 # Enable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=1

LightningOS-AOSP/vendor_lightning

config/common.mk
@@ -45,10 +45,12 @@ endif
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=0
+PRODUCT_PRODUCT_PROPERTIES += persist.sys.mist_debug_enabled=1
 else
 ifdef WITH_ADB_INSECURE
 # Forcebly disable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=0
+PRODUCT_PRODUCT_PROPERTIES += persist.sys.mist_debug_enabled=1
 else
 # Enable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=1